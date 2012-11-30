1b27a84783ae365f57e12a54b22efa37ebd9f828
Iain Ballard <iain.ballard@7digital.com>
2012-11-30 15:25:35 +0000

    Stub tests for type structure builder

--- src/SevenDigital.Messaging.Base.Unit.Tests/RoutingExample_IMetaData.cs
+++ src/SevenDigital.Messaging.Base.Unit.Tests/RoutingExample_IMetaData.cs
@@ -4,28 +4,41 @@ using SevenDigital.Messaging.Base;
 
 namespace Messaging.Base.Unit.Tests
 {
-	[TestFixture]
-    public class RoutingExample_IMetaData
-    {
+	[TestFixture, Ignore("NYI")]
+	public class RoutingExample_IMetaData
+	{
 		ITypeStructureRouter subject;
 		IMessageRouting router;
 
 		[SetUp]
-		public void A_routing_table_build_from_IMetadataFile ()
+		public void A_routing_table_build_from_IMetadataFile()
 		{
 			router = Substitute.For<IMessageRouting>();
+
 			subject = new TypeStructureRouter(router);
 
 			subject.BuildRoutes<Example.Types.IMetadataFile>();
 		}
 
 		[Test]
-		public void Should_create_source_for_IMetadataFile ()
+		public void Should_create_source_for_IMetadataFile()
 		{
-			Assert.Inconclusive();
+			router.Received().AddSource("Example.Types.IMetadataFile");
 		}
 
-    }
+		[Test]
+		public void Should_create_source_for_IFile()
+		{
+			router.Received().AddSource("Example.Types.IFile");
+		}
+
+		[Test]
+		public void Should_link_IMetadataFile_to_IFile_with_IFile_as_parent()
+		{
+			router.Received().RouteSources("Example.Types.IFile", "Example.Types.IMetadataFile", "");
+		}
+
+	}
 
 
 	public class TypeStructureRouter : ITypeStructureRouter
--- src/SevenDigital.Messaging.Base.Unit.Tests/TypeJunk/IMetadataFile.cs
+++ src/SevenDigital.Messaging.Base.Unit.Tests/TypeJunk/IMetadataFile.cs
@@ -1,6 +1,22 @@
 namespace Example.Types
 {
-	public interface IMetadataFile
+	public interface IMetadataFile : IFile
+	{
+	}
+
+	public interface IFile : IHash, IPath
+	{
+	}
+
+	public interface IPath: IMsg
+	{
+	}
+
+	public interface IHash: IMsg
+	{
+	}
+
+	public interface IMsg
 	{
 	}
 }
