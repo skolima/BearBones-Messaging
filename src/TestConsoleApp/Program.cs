af253ed018d795081b3e141d0d2f0dafc2a8b1ee
Iain Ballard <iain.ballard@7digital.com>
2012-12-04 11:27:36 +0000

    Signal capture test

---
+++ src/TestConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using SignalHandling;
+
+namespace TestConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Waiting for signal. Press ^C or send a SIGTERM (kill $pid)");
+			var waiter = new CrossPlatformSignalDispatch();
+			waiter.TerminateEvent += waiter_TerminateEvent;
+
+			while(true)
+			{
+				Thread.Sleep(250);
+			}
+		}
+
+		static void waiter_TerminateEvent(object sender, TerminateEventArgs args)
+		{
+			Console.WriteLine("Got a kill signal. Will now terminate");
+			Environment.Exit(0);
+		}
+	}
+}
---
+++ src/TestConsoleApp/ReadMe.md
@@ -0,0 +1,8 @@
+﻿Test Console App
+================
+
+This is used to test that the signal handling library works under Windows Console and Unix
+
+Press ^C in either and you should get a message and have the console app quit.
+
+Send a kill message in UNIX (e.g. with 'kill' and the test app's PID in the shell) and you should get the same result.
