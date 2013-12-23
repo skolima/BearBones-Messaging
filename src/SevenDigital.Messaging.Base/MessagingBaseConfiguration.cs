d7787201bf3366bb114603deb66d52a8dc7e253f
Leszek 'skolima' Ciesielski <leszek.ciesielski@7digital.com>
2013-12-23 14:18:54 +0000

    Removing deprecated code.

--- src/SevenDigital.Messaging.Base/MessagingBaseConfiguration.cs
+++ src/SevenDigital.Messaging.Base/MessagingBaseConfiguration.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Configuration;
 using SevenDigital.Messaging.Base.RabbitMq;
 using SevenDigital.Messaging.Base.RabbitMq.RabbitMqManagement;
 using SevenDigital.Messaging.Base.Routing;
@@ -46,31 +45,9 @@ namespace SevenDigital.Messaging.Base
 		}
 
 		/// <summary>
-		/// Use details contained in .Net configuration key `Messaging.Host` for connections
 		/// </summary>
-		[Obsolete("Please use WithConnection(IRabbitMqConnection connection) instead.")]
-		public MessagingBaseConfiguration WithConnectionFromAppConfig()
 		{
-			var parts = ConfigurationManager.AppSettings["Messaging.Host"].Split('/');
-			var hostUri = (parts.Length >= 1) ? (parts[0]) : ("localhost");
-			var vhost = (parts.Length >= 2 && parts[1].Length > 0) ? (parts[1]) : ("/");
 
-			return WithConnection(new RabbitMqConnection(hostUri, vhost));
-		}
-
-		/// <summary>
-		/// Use details contained in .Net configuration keys `Messaging.Host`, `ApiUsername` and `ApiPassword` for connections
-		/// </summary>
-		[Obsolete("Please use WithRabbitManagement(string host, string username, string password, string vhost) instead.")]
-		public MessagingBaseConfiguration WithRabbitManagementFromAppConfig()
-		{
-			var parts = ConfigurationManager.AppSettings["Messaging.Host"].Split('/');
-			var hostUri = (parts.Length >= 1) ? (parts[0]) : ("localhost");
-			var username = ConfigurationManager.AppSettings["ApiUsername"];
-			var password = ConfigurationManager.AppSettings["ApiPassword"];
-			var vhost = (parts.Length >= 2) ? (parts[1]) : ("/");
-
-			return WithRabbitManagement(hostUri, username, password, vhost);
 		}
 
 		const int port = 55672; // before RMQ 3; 3 redirects, so we use this for compatibility for now.
